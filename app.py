fa3b326a63ea3500cfa2fb68774319a5101c7d0b
ujangbudi <ujangbudi99@yahoo.com>
2025-03-24 01:29:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -90,4 +90,4 @@ def get_gemini_response(user_question):
         return f"Error dari Google Gemini: {response.status_code}"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
